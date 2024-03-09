762adb60a02b29271bc021e9ad022bc30bd11a8b
SAM PAZOUKI VAROONEH <68926038+sam-pazouki@users.noreply.github.com>
2024-03-09 19:15:26 -0300

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,9 @@ import { MemoryRouter } from 'react-router-dom';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<MemoryRouter>
+      <App />
+    </MemoryRouter>);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
